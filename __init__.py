5faf0bbe6a1fc5462f3a1a795160600dff4ba796
Peter O'Malley <ooomalley@gmail.com>
2014-02-21 21:19:44 -0800

    reworked config for dev/prod

--- __init__.py
+++ __init__.py
@@ -3,21 +3,19 @@ from flask.ext.assets import Environment, Bundle
 from flask.ext.sqlalchemy import SQLAlchemy
 from mongokit import Connection
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-MONGODB_DB = 'books'
-MONGODB_COLLECTION = 'books'
-T2_URL = '/t2/'
-#SERVER_NAME = 'localhost:5000'
-SERVER_NAME = 'tasker.physics.ucsb.edu'
-APPLICATION_ROOT = 'books'
+import os
+import config
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-                "postgresql://t2:rubypassword@localhost/t2_development"
+env = os.environ['FLASK_BOOKS_ENV'].lower()
+if env == 'dev' or env == 'development':
+    app.config.from_object(config.DevelopmentConfig)
+elif env == 'prod'or env == 'production':
+    app.config.from_object(config.ProductionConfig)
+else:
+    raise RuntimeError("environment variable FLASK_BOOKS_ENV not set to dev or prod.")
 app.secret_key = 'V\x1c\xd0\xc93\x9a7v\xbc\xa5\x1b\xaf[\xb77+\xed\xc7\xe8\t\xa0P\xba\x04'
-app.worldcat_key = 'cl3HqQBWmmZqi5tmLZJsmpypsjOhfdLrefItvpOOSvzqWmmKZb5WCvXz8WU7RYM0FcREHG0goYHMpGvQ'
+
 # config assets
 assets = Environment(app)
 #assets.url = app.static_url_path
---
+++ config.py
@@ -0,0 +1,21 @@
+''' config.py
+
+config info for flask/books
+'''
+
+class Config(object):
+    MONGODB_HOST = 'localhost'
+    MONGODB_PORT = 27017
+    MONGODB_DB = 'books'
+    MONGODB_COLLECTION = 'books'
+    #WORLDCAT_KEY = 'cl3HqQBWmmZqi5tmLZJsmpypsjOhfdLrefItvpOOSvzqWmmKZb5WCvXz8WU7RYM0FcREHG0goYHMpGvQ'
+
+class DevelopmentConfig(Config):
+    T2_URL = 'http://localhost:3000/'
+    SERVER_NAME = 'localhost:5000'
+    SQLALCHEMY_DATABASE_URI = "postgresql://t2:rubypassword@localhost/t2_development"
+
+class ProductionConfig(Config):
+    T2_URL = '/t2/'
+    SERVER_NAME = 'tasker.physics.ucsb.edu'
+    SQLALCHEMY_DATABASE_URI = "postgresql://t2:rubypassword@localhost/t2_production"
